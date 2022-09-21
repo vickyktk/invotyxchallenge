555ed516f84190687dcf62ed0a729da6708b4936
vickyktk <waqasktk81@gmail.com>
2022-09-21 21:15:19 +0500

    Swagger documentaion added

--- backend_challenge/index.js
+++ backend_challenge/index.js
@@ -6,12 +6,19 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 const connectDB = require('./model/db')
 connectDB()
+const swaggerUi = require('swagger-ui-express')
+
+const swaggerDocument = require('./swagger.json');
 
 const apiRoutes = require('./routes/Routes')
 const verfiyAuth = require('./middlewares/middleware')
 
 app.use("/api/v1", verfiyAuth, apiRoutes);
-
+app.use(
+  '/api-docs',
+  swaggerUi.serve, 
+  swaggerUi.setup(swaggerDocument)
+);
 app.listen(PORT, () => {
   console.log('Listening on port', PORT);
 });
